eb74aa6ec17ed4d8ada0f410e8238b0f79c4206d
Joalisson Barros <joalissonricardo@gmail.com>
2023-09-25 10:51:56 -0300

    Init framework

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use App\Contracts\Repositories\AccountRepositoryInterface;
+use App\Repository\AccountRepository;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(AccountRepositoryInterface::class, AccountRepository::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
